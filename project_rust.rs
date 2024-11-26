f8ae52b4ff333fa43b4c2648c7bbc533f510c23b
Fahim <fahim77.ff@gmail.com>
2024-11-26 15:22:48 +0100

    V1

--- project_rust.rs
+++ project_rust.rs
@@ -1,2 +1,23 @@
 #![no_std]
 #![no_main]
+
+
+use core::ptr;
+
+unsafe fn reserve_memory(size: usize) -> *mut u8 {
+
+    let mut base_address: usize = 0;
+    let mut new_address: usize = 0;
+
+    asm!(
+        "mov rax, 12",           
+        "xor rdi, rdi",          
+        "syscall",
+        "mov {}, rax",           
+        out(reg) base_address
+    );
+
+
+
+}
+
