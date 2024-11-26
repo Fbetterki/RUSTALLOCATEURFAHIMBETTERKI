0dd7391c6013bdb625944ea4b55bbde51e38fdba
Fahim <fahim77.ff@gmail.com>
2024-11-26 16:05:04 +0100

    V3

--- project_rust.rs
+++ project_rust.rs
@@ -4,8 +4,8 @@
 
 use core::ptr;
 
-unsafe fn reserve_memory(size: usize) -> *mut u8 {
-
+unsafe fn reserve_memory(size: usize) -> *mut u8 
+{
     let mut base_address: usize = 0;
     let mut new_address: usize = 0;
 
@@ -17,11 +17,24 @@ unsafe fn reserve_memory(size: usize) -> *mut u8 {
         out(reg) base_address
     );
 
-
     if base_address == 0 {
         return ptr::null_mut(); 
     }
 
 
+    let target_address = base_address + size;
+    asm!(
+        "mov rax, 12",           
+        "mov rdi, {}",           
+        "syscall",
+        "mov {}, rax",           
+        in(reg) target_address, out(reg) new_address
+    );
+
+    if new_address < target_address {
+        return ptr::null_mut(); 
+    }
+
+    base_address as *mut u8
 }
 
